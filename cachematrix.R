27f6944231c24561712764c272706f3fc36e414b
Gabriel Armelin <gabrielbma@gmail.com>
2014-05-21 21:10:31 -0400

    implemented makeCacheMatrix() and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains functions responsible for the computation of the 
+## inverse of a special square "matrix" and caching its results to improve 
+## computation speed in subsequent calls.
 
-## Write a short comment describing this function
-
+## This function wraps a regular R square matrix in order to add 
+## cache capabilities. The new wrapped matrix contains methods to
+## set a matrix, to get the original matrix, to get the inverse matrix
+## and to set the inverse matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function is responsible for the computation of inverse of a matrix 
+## retrieved from makeCacheMatrix() and caching the result so that subsequent 
+## calls to this function returns the result already computed previously.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
